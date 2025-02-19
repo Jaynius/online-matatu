04e484570a01c433a84cf599f793acbe4bf7cf96
jaynius <bramweljumab@gmail.com>
2025-02-19 06:21:49 +0300

    bootstrap project backend

---
+++ src/main/java/com/benjamin/onlinematatu/OnlineMatatuApplication.java
@@ -0,0 +1,13 @@
+package com.benjamin.onlinematatu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMatatuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMatatuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/benjamin/onlinematatu/OnlineMatatuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjamin.onlinematatu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMatatuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
