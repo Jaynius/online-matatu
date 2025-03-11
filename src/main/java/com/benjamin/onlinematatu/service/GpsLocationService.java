8ec4ef192f9e2c3e1bc24408dce6578d47ab54a3
bram <bramweljumab@gmail.com>
2025-03-11 14:13:35 +0300

    service layer development
    Took 11 minutes

--- src/main/java/com/benjamin/onlinematatu/service/GpsLocationService.java
+++ src/main/java/com/benjamin/onlinematatu/service/GpsLocationService.java
@@ -1,7 +1,13 @@
 package com.benjamin.onlinematatu.service;
 
+import com.benjamin.onlinematatu.entity.GpsLocation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface GpsLocationService {
+
+    private ResponseEntity addLocation() {
+        return null;
+    }
 }
